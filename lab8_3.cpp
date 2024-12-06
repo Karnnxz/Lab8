2b1b980a195ae942df9da7f9e166fb41b329f14c
karnnxz <garnt49@gmail.com>
2024-12-06 15:38:35 +0700

    lab8_3.cpp

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,58 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int Age, Height, Bounty;
+
+    cout << "Enter your age: ";
+    cin >> Age;
+
+    if (Age <=25) {
+
+        cout << "Enter your height: ";
+        cin >> Height;
+
+        if (Height < 100) {
+
+            cout << "Your character = Chopper";
+
+        } else if (Height < 180) {
+
+            cout << "Your character = Usopp";
+
+        } else {
+
+            cout << "Enter your bounty: ";
+            cin >> Bounty;
+
+            if (Bounty > 1100000000) {
+
+            cout << "Your character = Zoro";
+            
+            } else {
+
+            cout << "Your character = Sanji";
+
+            }
+
+        }
+
+    } else if (Age <= 60) {
+
+        cout << "Enter your bounty: ";
+        cin >> Bounty;
+
+        if (Bounty > 500000000) {
+            cout << "Your character = Jinbe";
+        } else {
+            cout << "Your character = Franky";
+        }
+        
+    } else {
+
+        cout << "Your character = Brook";
+
+    }
+
+}
